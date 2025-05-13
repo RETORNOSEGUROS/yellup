aa110175904ba67c65431d7c92aaa6456df60999
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-05-13 15:20:06 -0300

    Update torcida.js

--- scripts/torcida.js
+++ scripts/torcida.js
@@ -10,7 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "75092713724",
   appId: "1:75092713724:web:EXEMPLOID"
 };
-
   apiKey: "SUA_API_KEY",
   authDomain: "painel-yellup.firebaseapp.com",
   projectId: "painel-yellup",
