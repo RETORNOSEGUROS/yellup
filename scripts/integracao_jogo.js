ef02c519d3b64bfa4105209cd947e35107b58f5f
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-05-14 15:09:35 -0300

    Add files via upload

--- scripts/integracao_jogo.js
+++ scripts/integracao_jogo.js
@@ -1,1 +1,16 @@
-// Integração jogo usuário
+
+import { db } from '../firebase/firebase-config.js';
+
+// Função para carregar dados do jogo
+export function carregarJogo(jogoId) {
+  const jogoRef = db.collection("jogos").doc(jogoId);
+  return jogoRef.get().then((doc) => {
+    if (doc.exists) {
+      return doc.data();
+    } else {
+      throw new Error("Jogo não encontrado");
+    }
+  });
+}
+
+// Exemplo de uso: carregarJogo('idDoJogo').then(jogo => console.log(jogo));
