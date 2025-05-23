4b810697673a4bb584112d4ed41bd02d30e93f96
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-05-23 14:47:12 -0300

    Add files via upload

---
+++ admin/firebase-init.js
@@ -0,0 +1,13 @@
+// firebase-init.js (usando compat SDK)
+const firebaseConfig = {
+  apiKey: "AIzaSyC5ZrkEy7KuCFJOtPvI7-P-JcA0MF4im5c",
+  authDomain: "painel-yellup.firebaseapp.com",
+  projectId: "painel-yellup",
+  storageBucket: "painel-yellup.firebasestorage.app",
+  messagingSenderId: "608347210297",
+  appId: "1:608347210297:web:75092713724e617c7203e8",
+  measurementId: "G-SYZ16X31KQ"
+};
+
+// Inicializa o Firebase com compat (para Firestore 9 compatível)
+firebase.initializeApp(firebaseConfig);
