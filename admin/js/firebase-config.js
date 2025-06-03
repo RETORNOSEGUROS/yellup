a34b1461513e637ed86aeabc6fa5807e0e610834
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-06-03 09:43:02 -0300

    Update firebase-config.js

--- admin/js/firebase-config.js
+++ admin/js/firebase-config.js
@@ -1,5 +1,3 @@
-// Configuração Firebase compatível (modo compatível)
-
 const firebaseConfig = {
   apiKey: "AIzaSyD7Qo8A62FC1dMH6ugfChfVTxpFET2nD7k",
   authDomain: "painel-retorno.firebaseapp.com",
@@ -9,5 +7,6 @@ const firebaseConfig = {
   appId: "1:568615665836:web:cf4b053cf7911dbe122661"
 };
 
+// Inicializa o Firebase no modo compatível
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
