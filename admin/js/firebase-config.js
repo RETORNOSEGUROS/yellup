fea76d32da9b388512b97d6d4c45a5ead16ff8f5
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-06-03 10:51:02 -0300

    Update firebase-config.js

--- admin/js/firebase-config.js
+++ admin/js/firebase-config.js
@@ -1,10 +1,12 @@
 const firebaseConfig = {
   apiKey: "AIzaSyD7Qo8A62FC1dMH6ugfChfVTxpFET2nD7k",
-  authDomain: "painel-yellup.firebaseapp.com",,
+  authDomain: "painel-yellup.firebaseapp.com",
   projectId: "painel-yellup",
   storageBucket: "painel-yellup.appspot.com",
   messagingSenderId: "568615665836",
   appId: "1:568615665836:web:cf4b053cf7911dbe122661"
 };
+
+// Inicializa o Firebase no modo compatível
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
