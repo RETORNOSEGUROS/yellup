f20d91edf2859d2ec54cdde9d7f0a8cdbc093e43
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-06-15 13:29:59 -0300

    Add files via upload

---
+++ admin/js/firebase-init .js
@@ -0,0 +1,14 @@
+// firebase-init.js (100% compatível com HTML direto)
+const firebaseConfig = {
+  apiKey: "AIzaSyC5ZrkEy7KuCFJOtPvI7-P-JcA0MF4im5c",
+  authDomain: "painel-yellup.firebaseapp.com",
+  projectId: "painel-yellup",
+  storageBucket: "painel-yellup.firebasestorage.app",
+  messagingSenderId: "608347210297",
+  appId: "1:608347210297:web:75092713724e617c7203e8",
+  measurementId: "G-SYZ16X31KQ"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
