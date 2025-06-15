de6f3f3cd6a1be1568139223ee823f14df3b3252
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-06-15 14:25:59 -0300

    Update firebase-init .js

--- admin/js/firebase-init .js
+++ admin/js/firebase-init .js
@@ -1,12 +1,11 @@
-// Arquivo firebase-init.js
 const firebaseConfig = {
-  apiKey: "AIzaSyC5ZrkEy7KuCFJOtPvI7-P-JcA0MF4im5c",
-  authDomain: "painel-yellup.firebaseapp.com",
-  projectId: "painel-yellup",
-  storageBucket: "painel-yellup.firebasestorage.app",
-  messagingSenderId: "608347210297",
-  appId: "1:608347210297:web:75092713724e617c7203e8",
-  measurementId: "G-SYZ16X31KQ"
+    apiKey: "AIzaSyC5ZrkEy7KuCFJOtPvI7-P-JcA0MF4im5c",
+    authDomain: "painel-yellup.firebaseapp.com",
+    projectId: "painel-yellup",
+    storageBucket: "painel-yellup.firebasestorage.app",
+    messagingSenderId: "608347210297",
+    appId: "1:608347210297:web:75092713724e617c7203e8",
+    measurementId: "G-SYZ16X31KQ"
 };
 
 firebase.initializeApp(firebaseConfig);
