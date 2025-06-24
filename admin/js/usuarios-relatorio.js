c5ac2f1a1fe804c8bb1403c349aa31df7b94b72d
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-06-24 16:56:56 -0300

    Add files via upload

---
+++ admin/js/usuarios-relatorio.js
@@ -0,0 +1,2 @@
+// Script do relatório será implementado aqui futuramente
+console.log("usuarios-relatorio.js carregado corretamente. Em breve será implementada a lógica dos filtros e exportações.");
