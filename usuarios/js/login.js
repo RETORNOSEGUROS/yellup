918b6c7f34607a7ba209f2d04bd435db4402a7ff
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-07-02 21:45:00 -0300

    Update login.js

--- usuarios/js/login.js
+++ usuarios/js/login.js
@@ -4,6 +4,6 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
   const email = document.getElementById("email").value;
   const senha = document.getElementById("senha").value;
   auth.signInWithEmailAndPassword(email, senha)
-    .then(() => alert("Login bem-sucedido!"))
+    .then(() => window.location.href = "painel.html";
     .catch(error => alert("Erro: " + error.message));
 });
