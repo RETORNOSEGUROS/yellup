53b8f161a591c72861d0aeb383500c861991b68e
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-07-07 16:06:19 -0300

    Update firebase-init.js

--- usuarios/js/firebase-init.js
+++ usuarios/js/firebase-init.js
@@ -1,14 +1,12 @@
-// Inicialização Firebase compatível com v9.22.2 + compat
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "AIzaSyC5ZrkEy7KuCFJOtPvI7-P-JcA0MF4im5c",
   authDomain: "painel-yellup.firebaseapp.com",
   projectId: "painel-yellup",
-  storageBucket: "painel-yellup.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  storageBucket: "painel-yellup.firebasestorage.app",
+  messagingSenderId: "608347210297",
+  appId: "1:608347210297:web:75092713724e617c7203e8",
+  measurementId: "G-SYZ16X31KQ"
 };
 
 firebase.initializeApp(firebaseConfig);
-
-// Use firebase.auth() diretamente (v9 compat usa o mesmo padrão do v8)
 const db = firebase.firestore();
